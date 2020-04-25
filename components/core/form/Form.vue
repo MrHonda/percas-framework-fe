2b69273f310c7c1ca8cc655b869bfe6e618ec937
MrHonda <>
2020-04-25 20:18:27 +0200

    Form

--- components/core/form/Form.vue
+++ components/core/form/Form.vue
@@ -1,44 +1,88 @@
 <template>
   <v-form>
-    <v-tabs>
-      <v-tab v-for="i in 3" :key="i" :href="`#tab-${i}`">
-        Tab {{i}}
-      </v-tab>
-      <v-tab-item v-for="i in 3" :key="i" :value="`tab-${i}`" :transition="false" :reverse-transition="false">
-        <v-container>
-          Tab {{i}}
-          <v-row v-for="i in 4" :key="i">
-            <v-col>
-              <v-text-field outlined label="Label" prepend-inner-icon="mdi-tag-text-outline" clearable dense>
-              </v-text-field>
-            </v-col>
-            <v-col>
-              <v-text-field outlined label="Label" prepend-inner-icon="mdi-tag-text-outline" clearable dense>
-              </v-text-field>
-            </v-col>
-          </v-row>
-          <v-row justify="end">
-            <v-btn
-              color="success"
-              class="mr-2"
-            >
-              <v-icon left>mdi-content-save</v-icon>
-              Save
-            </v-btn>
-            <v-btn text>
-              <v-icon left>mdi-close</v-icon>
-              Cancel
-            </v-btn>
-          </v-row>
-        </v-container>
-      </v-tab-item>
-    </v-tabs>
+    <div class="headline py-2 px-1" v-html="title"></div>
+    <v-container v-for="(part, partName) in parts" :key="partName">
+      <v-row v-for="(row, rowIndex) in part" :key="rowIndex">
+        <v-col v-for="field in row" :key="field.name" :cols="12" :md="field.col">
+          <v-text-field outlined :label="field.text" :prepend-inner-icon="field.icon" dense :value="field.value" v-model="field.value">
+          </v-text-field>
+        </v-col>
+      </v-row>
+    </v-container>
+    <v-container>
+      <v-row justify="end">
+        <v-btn
+          v-for="action in actions"
+          :key="action.name"
+          :color="action.color"
+          :outlined="action.style.outlined"
+          :text="action.style.text"
+          class="mr-2"
+          @click="emitAction(action.name)"
+        >
+          <v-icon left>{{action.icon}}</v-icon>
+          {{action.text}}
+        </v-btn>
+      </v-row>
+    </v-container>
+<!--    <v-tabs>-->
+<!--      <v-tab v-for="i in 3" :key="i" :href="`#tab-${i}`">-->
+<!--        Tab {{i}}-->
+<!--      </v-tab>-->
+<!--      <v-tab-item v-for="i in 3" :key="i" :value="`tab-${i}`" :transition="false" :reverse-transition="false">-->
+<!--        <v-container>-->
+<!--          Tab {{i}}-->
+<!--          <v-row v-for="i in 4" :key="i">-->
+<!--            <v-col>-->
+<!--              <v-text-field outlined label="Label" prepend-inner-icon="mdi-tag-text-outline" clearable dense>-->
+<!--              </v-text-field>-->
+<!--            </v-col>-->
+<!--            <v-col>-->
+<!--              <v-text-field outlined label="Label" prepend-inner-icon="mdi-tag-text-outline" clearable dense>-->
+<!--              </v-text-field>-->
+<!--            </v-col>-->
+<!--          </v-row>-->
+<!--          <v-row justify="end">-->
+<!--            <v-btn-->
+<!--              color="success"-->
+<!--              class="mr-2"-->
+<!--            >-->
+<!--              <v-icon left>mdi-content-save</v-icon>-->
+<!--              Save-->
+<!--            </v-btn>-->
+<!--            <v-btn text>-->
+<!--              <v-icon left>mdi-close</v-icon>-->
+<!--              Cancel-->
+<!--            </v-btn>-->
+<!--          </v-row>-->
+<!--        </v-container>-->
+<!--      </v-tab-item>-->
+<!--    </v-tabs>-->
   </v-form>
 </template>
 
 <script>
   export default {
-    name: 'Form'
+    name: 'Form',
+    props: {
+      title: {
+        type: String,
+        required: true
+      },
+      parts: {
+        type: Object,
+        required: true
+      },
+      actions: {
+        type: Array,
+        required: true
+      }
+    },
+    methods: {
+      emitAction(action) {
+        console.log(action);
+      }
+    }
   }
 </script>
 
---
+++ pages/system/modules/_id.vue
@@ -0,0 +1,52 @@
+<template>
+  <div>
+    <Form
+      v-bind="form"
+    />
+  </div>
+</template>
+
+<script>
+  import Form from '~/components/core/form/Form';
+
+  export default {
+    components: {Form},
+    data() {
+      return {
+        form: {
+          title: 'Form',
+          parts: {
+            'part1': [
+              [
+                {
+                  name: 'name',
+                  type: 'text',
+                  text: 'Name',
+                  icon: 'mdi-text',
+                  value: 'system',
+                  col: 6
+                },
+                {
+                  name: 'link',
+                  type: 'text',
+                  text: 'Link',
+                  icon: 'mdi-text',
+                  value: '/system',
+                  col: 6
+                }
+              ]
+            ]
+          },
+          actions: [
+            {name: 'save', icon: 'mdi-content-save', text: 'Save', color: 'primary', style: {}},
+            {name: 'close', icon: 'mdi-close', text: 'Close', color: '', style: {text: true}}
+          ]
+        }
+      }
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
