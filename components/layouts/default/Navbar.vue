a6340aa0591943af21ef71ce73e85fc1ea3dec26
MrHonda <hondacze@gmail.com>
2020-06-13 13:07:06 +0200

    Default layout

---
+++ components/layouts/default/Navbar.vue
@@ -0,0 +1,65 @@
+<template>
+  <v-app-bar
+    app
+    color="primary"
+    flat
+    clipped-left
+    dark
+  >
+    <v-toolbar-title class="navbar--title">
+      PERCAS
+    </v-toolbar-title>
+    <div class="navbar--module">
+      <v-menu offset-y>
+        <template v-slot:activator="{ on, attrs }">
+          <v-btn
+            v-bind="attrs"
+            v-on="on"
+            text
+          >
+            <v-icon left>mdi-home</v-icon>
+            Home
+            <v-icon right>mdi-chevron-down</v-icon>
+          </v-btn>
+        </template>
+        <v-list dense>
+          <v-list-item-group color="primary">
+            <v-list-item to="/home" router>
+              <v-list-item-icon>
+                <v-icon>mdi-home</v-icon>
+              </v-list-item-icon>
+              <v-list-item-content>
+                <v-list-item-title>Home</v-list-item-title>
+              </v-list-item-content>
+            </v-list-item>
+            <v-list-item to="/home2">
+              <v-list-item-icon>
+                <v-icon>mdi-home</v-icon>
+              </v-list-item-icon>
+              <v-list-item-content>
+                <v-list-item-title>Home</v-list-item-title>
+              </v-list-item-content>
+            </v-list-item>
+          </v-list-item-group>
+        </v-list>
+      </v-menu>
+    </div>
+  </v-app-bar>
+</template>
+
+<script>
+export default {
+  name: 'Navbar'
+}
+</script>
+
+<style scoped lang="scss">
+.navbar--title {
+  width: calc(#{$sidebar-width} - 16px);
+  padding-right: 16px;
+  font-size: 24px;
+  font-weight: 500;
+  cursor: default;
+  text-align: center;
+}
+</style>
---
+++ components/layouts/default/Sidebar.vue
@@ -0,0 +1,18 @@
+<template>
+  <v-navigation-drawer
+    app
+    clipped
+  >
+
+  </v-navigation-drawer>
+</template>
+
+<script>
+export default {
+  name: 'Sidebar'
+}
+</script>
+
+<style scoped>
+
+</style>
