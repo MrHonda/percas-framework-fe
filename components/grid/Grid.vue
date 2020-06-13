23027c6f08c1b5aad009d0306c27563324522c25
MrHonda <>
2020-06-13 15:14:49 +0200

    Basic grid

---
+++ components/grid/Grid.vue
@@ -0,0 +1,54 @@
+<template>
+  <v-data-table
+    :headers="grid.headers"
+    :items="grid.items"
+  >
+    <template v-slot:item.actions="{ item }">
+      <v-menu
+        v-if="item.actions && item.actions.length"
+        offset-y
+        left
+      >
+        <template v-slot:activator="{ on, attrs }">
+          <v-btn
+            v-bind="attrs"
+            v-on="on"
+            icon
+          >
+            <v-icon>mdi-dots-vertical</v-icon>
+          </v-btn>
+        </template>
+        <v-list>
+          <v-list-item
+            v-for="action in item.actions"
+            :key="action.key"
+            link
+          >
+            <v-list-item-icon>
+              <v-icon v-text="action.icon" />
+            </v-list-item-icon>
+            <v-list-item-content>
+              <v-list-item-title v-text="action.name" />
+            </v-list-item-content>
+          </v-list-item>
+        </v-list>
+      </v-menu>
+    </template>
+  </v-data-table>
+</template>
+
+<script>
+export default {
+  name: 'Grid',
+  props: {
+    grid: {
+      type: Object,
+      required: true
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ pages/system/modules/index.vue
@@ -0,0 +1,75 @@
+<template>
+  <v-container>
+    <h1>Modules</h1>
+    <Grid
+      :grid="grid"
+    />
+  </v-container>
+</template>
+
+<script>
+
+import Grid from '~/components/grid/Grid';
+
+export default {
+  components: {Grid},
+  data() {
+    return {
+      grid: null
+    }
+  },
+  created() {
+    this.grid = {
+      headers: [
+        {
+          text: 'Name',
+          value: 'name'
+        },
+        {
+          text: 'Link',
+          value: 'link'
+        },
+        {
+          text: '',
+          value: 'actions',
+          width: 100
+        }
+      ],
+      items: [
+        {
+          name: 'Module 1',
+          link: 'module-1',
+          actions: [
+            {
+              'key': 'edit',
+              'name': 'Edit',
+              'icon': 'mdi-pencil'
+            },
+            {
+              'key': 'delete',
+              'name': 'Delete',
+              'icon': 'mdi-delete'
+            }
+          ]
+        },
+        {
+          name: 'Module 2',
+          link: 'module-2',
+          actions: [
+            {
+              'key': 'edit',
+              'name': 'Edit',
+              'icon': 'mdi-pencil'
+            },
+            {
+              'key': 'delete',
+              'name': 'Delete',
+              'icon': 'mdi-delete'
+            }
+          ]
+        }
+      ]
+    };
+  }
+}
+</script>
